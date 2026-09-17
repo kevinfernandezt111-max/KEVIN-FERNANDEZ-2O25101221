1a2bd4c641794b86d8ce52e0108172b3a6995751
Laboratorio Asu <laboratorioasu@unida.edu.py>
2026-09-17 19:25:40 -0300

    eje

---
+++ KEVIN EJE1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+cout << "KLK mi gente"
+//HOL como Vas hACE TIEMPO NOSE DE TI Y SE QUE NO SOMOS NA 
+
+	
+	return (0);
+}
--- ej2.cpp
+++ ej2.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std ;
+
+//prototipo de las funciones
 float suma( float a , float b);
 float resta( float a , float b);
 float multiplicacion( float a , float b);
 float division( float a , float b);
 
 int main(){
+	// declaracion
 	float suma_denumeros,resta_denumeros,multiplicacion_denumeros,division_denumeros,n1,n2;
 	 n1=2;
 	 n2=5;
 	 
+	 // llamar a las funciones
 	  suma_denumeros = suma( n1 ,n2);
 
       resta_denumeros = resta( n1 ,n2);
@@ -20,6 +24,7 @@ int main(){
       division_denumeros = division( n1 ,n2);
 	 
 	 
+// impresion de resultados
 
 	cout <<"la suma de "<<n1<<" con "<<n2<<"es igual a "<<suma_denumeros<<"\n";
 	cout <<"la resta de "<<n1<<" con "<<n2<<"es igual a "<<resta_denumeros<<"\n";
@@ -28,6 +33,8 @@ int main(){
 	system("PAUSE");
 	
 }
+// llamar de alas funciones para la logica del resultado
+
 float suma( float a , float b) {
 return a + b; }
 float resta( float a , float b) {
